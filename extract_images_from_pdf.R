40cf47b112626b272a25d4453a55a29644af1c56
Meseth <n.meseth@hs-osnabrueck.de>
2023-06-29 23:29:07 +0200

    automatic push from script

--- extract_images_from_pdf.R
+++ extract_images_from_pdf.R
@@ -1,16 +1,26 @@
 library(pdftools)
 library(magick)
+library(stringr)
 
-pdf_path <- "docs/google_slides/LiFi-Project - Program's Anatomy.pdf"
-pdf_info <- pdf_info(pdf_path)
-
-# Extract the total number of pages/slides
-total_slides <- pdf_info$pages
-
-for (p in 1:total_slides) {
-
-  image <- image_read_pdf(pdf_path, pages = p )
-  image_path = paste0("docs/images/img_", p, ".png")
-  unlink(image_path)
-  image_write(image, image_path)
+extract_images <- function(pdf_path) {
+  total_slides <- pdf_info$pages
+  
+  pdf_text <- pdf_text(pdf_path)
+  
+  for (p in 1:total_slides) {
+    
+    # Get the image file name from the secret code on the slide (if present)
+    image_file_name <- str_extract(pdf_text[p], "(?<=%%).*?(?=%%)")
+    
+    if(!is.na(image_file_name)) {
+      image <- image_read_pdf(pdf_path, pages = p )
+      image_path = paste0("docs/images/", image_file_name, ".png")
+      unlink(image_path)
+      image_write(image, image_path)
+    }
+  }
 }
+
+pdf_path <- "docs/google_slides/Problem Solving and Computational Thinking.pdf"
+extract_images(pdf_path)
+
