8c41df4537e0a96a669d53dfed03026abd8c8882
Meseth <n.meseth@hs-osnabrueck.de>
2023-06-29 23:54:08 +0200

    automatic push from script

---
+++ build.R
@@ -0,0 +1,9 @@
+# Docs
+source("download_google_docs.R")
+
+# Slides
+source("download_google_slides.R")
+source("extract_images_from_pdf.R")
+
+# Push
+source("push.R")
--- extract_images_from_pdf.R
+++ extract_images_from_pdf.R
@@ -3,14 +3,19 @@ library(magick)
 library(stringr)
 
 extract_images <- function(pdf_path) {
+  pdf_info <- pdf_info(pdf_path)
   total_slides <- pdf_info$pages
   
   pdf_text <- pdf_text(pdf_path)
+  
+  #print(pdf_text)
   
   for (p in 1:total_slides) {
     
     # Get the image file name from the secret code on the slide (if present)
     image_file_name <- str_extract(pdf_text[p], "(?<=%%).*?(?=%%)")
+    
+    #print(image_file_name)
     
     if(!is.na(image_file_name)) {
       image <- image_read_pdf(pdf_path, pages = p )
@@ -21,6 +26,14 @@ extract_images <- function(pdf_path) {
   }
 }
 
-pdf_path <- "docs/google_slides/Problem Solving and Computational Thinking.pdf"
-extract_images(pdf_path)
+path <- "docs/google_slides"
+files <- list.files(path, full.names = TRUE, recursive = T)
+files
+
+for (file in files) {
+  if(endsWith(file, ".pdf")) {
+    extract_images(file)
+  }
+  
+}
 
