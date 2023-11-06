f6b2913fb01fedaa5f3d5fe4c6cd600f22414676
Meseth <n.meseth@hs-osnabrueck.de>
2023-11-06 10:29:37 +0100

    automatic push from script

--- build.R
+++ build.R
@@ -5,20 +5,42 @@ if (dir_exists(output_path)) {
   dir_delete(output_path)
 }
 dir_create(output_path)
-  
+
+if (exists("build_steps")) {
+  print("Running defined build steps only.")
+} else {
+  print("Running all build steps")
+  build_steps <- c("google_slides", "google_docs", "quarto", "extract_images", "create_index", "push")
+}
+
 # Docs
-source("build_scripts/download_google_docs.R")
+if("google_docs" %in% build_steps) {
+  source("build_scripts/download_google_docs.R")
+}
+
 
 # Slides
-source("build_scripts/download_google_slides.R")
-source("build_scripts/extract_images_from_pdf.R")
+if("google_slides" %in% build_steps) {
+  source("build_scripts/download_google_slides.R")
+}
+
+# Images from Slides
+if("extract_images" %in% build_steps) {
+  source("build_scripts/extract_images_from_pdf.R")
+}
 
 # Quarto
-source("build_scripts/render_and_copy_quarto.R")
+if("quarto" %in% build_steps) {
+  source("build_scripts/render_and_copy_quarto.R")
+}
 
-source("build_scripts/create_index.R")
+if("create_index" %in% build_steps) {
+  source("build_scripts/create_index.R")
+}
 
 # Push
-source("build_scripts/push.R")
+if("push" %in% build_steps) {
+  source("build_scripts/push.R")
+}
 
 print("Done: https://winf-hsos.github.io/university-docs/")
