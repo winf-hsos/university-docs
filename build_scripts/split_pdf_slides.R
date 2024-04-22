c3e83748fc88f59b114d12d8e02ffa8d2eb27952
Meseth <n.meseth@hs-osnabrueck.de>
2024-04-22 09:59:55 +0200

    automatic push from script

--- build_scripts/split_pdf_slides.R
+++ build_scripts/split_pdf_slides.R
@@ -7,13 +7,16 @@ split_pdf <- function(pdf_path) {
   # Extract text from each page
   text_pages <- pdf_text(pdf_path)
   
+  # Get the base filename without extension
+  base_filename <- tools::file_path_sans_ext(basename(pdf_path))
+  
   # Initialize list to store split points and filenames
   split_points <- list()
   
   # Loop over pages to find markers and extract filenames
   for (i in seq_along(text_pages)) {
     # Find markers
-    markers <- str_extract_all(text_pages[i], "%!.*?!%")[[1]]
+    markers <- str_extract_all(text_pages[i], "(?<=%!).*?(?=%!)")[[1]]
     if (length(markers) > 0) {
       # Extract filename and add to list with the page number
       filenames <- str_replace_all(markers, "%!|%!", "")
@@ -28,16 +31,14 @@ split_pdf <- function(pdf_path) {
   for (filename in names(split_points)) {
     end_page <- split_points[[filename]] - 1
     if (start_page <= end_page) {
-      pdf_subset(pdf_path, pages = start_page:end_page, output = paste0(filename, ".pdf"))
+      output_folder <- dirname(pdf_path)
+      new_filename <- paste0(base_filename, " - ", filename, ".pdf")
+      pdf_subset(pdf_path, pages = start_page:end_page, output = file.path(output_folder, paste0(new_filename)))
     }
     start_page <- split_points[[filename]] + 1
   }
   
-  # Check if there are pages after the last marker
-  if (start_page <= length(text_pages)) {
-    pdf_subset(pdf_path, pages = start_page:length(text_pages), output = "remaining_pages.pdf")
-  }
 }
 
 # Example usage
-split_pdf("path_to_your_pdf.pdf")
+split_pdf("docs/google_slides/wirtschaftsinformatik/Digitization - Information Representation and Processing.pdf")
