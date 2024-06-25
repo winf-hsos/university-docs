55e879b79abecfcd47dcf6660af2131311ea7dde
Meseth <n.meseth@hs-osnabrueck.de>
2024-06-25 16:53:15 +0200

    automatic push from script

--- build_quarto_only.R
+++ build_quarto_only.R
@@ -1,7 +1,7 @@
 source("build_scripts/utils.R")
 
 # Quarto
-source("build_scripts/render_and_copy_quarto_delta.R")
+source("build_scripts/render_and_copy_quarto_force_all.R")
 
 # Push
 source("build_scripts/push.R")
--- build_scripts/render_and_copy_quarto_force_all.R
+++ build_scripts/render_and_copy_quarto_force_all.R
@@ -1,9 +1,26 @@
-library(quarto)
+library(quarto)  # Load the quarto package if not already loaded
 
-# Render and copy course material ####
-qmd_list <- list.files("quarto", ".qmd", full.names = TRUE, recursive = TRUE)
-quarto_render(qmd_list, output_format = "pdf", quiet = TRUE)
+# Define the directory where you want to search for ".qmd" files
+base_directory <- "quarto"
 
+# List all ".qmd" files in the directory and its subdirectories
+qmd_files <- list.files(base_directory, pattern = "\\.qmd$", recursive = TRUE, full.names = TRUE)
+
+
+# Iterate through the list of ".qmd" files
+for (qmd_file in qmd_files) {
+  # Construct the corresponding ".pdf" file path
+  pdf_file <- sub("\\.qmd$", ".pdf", qmd_file)
+  
+  # Execute the "render_quarto()" function with the ".qmd" file
+  cat("Start rendering: ", qmd_file, "\n")
+  quarto_render(qmd_file, output_format = "pdf", quiet = TRUE)
+  cat("Finished rendering: ", qmd_file, "\n")
+
+}
+
+cat("Done with rendering Quarto files!\n")
+cat("Copying PDF files to output foleder\n")
 
 # Get all PDF files
 pdf_list <- list.files("quarto", ".pdf", full.names = TRUE, recursive = TRUE)
@@ -15,6 +32,8 @@ dir.create("docs/quarto")
 dir.create("docs/quarto/web_engineering")
 dir.create("docs/quarto/information_management")
 dir.create("docs/quarto/applied_analytics")
+dir.create("docs/quarto/data_analytics")
+dir.create("docs/quarto/digitization_and_programming")
 
 # Copy PDF files
 file.copy(pdf_list, pdf_dest_list, overwrite = TRUE)
