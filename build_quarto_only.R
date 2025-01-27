e737a18efc9fc8f1e5f33cc02fa3dd725bd752b8
Meseth <n.meseth@hs-osnabrueck.de>
2025-01-27 16:36:37 +0100

    automatic push from script

--- build_quarto_only.R
+++ build_quarto_only.R
@@ -1,7 +1,10 @@
 source("build_scripts/utils.R")
 
 # Quarto
-source("build_scripts/render_and_copy_quarto_force_all.R")
+#source("build_scripts/render_and_copy_quarto_force_all.R")
+
+source("build_scripts/render_and_copy_quarto_delta.R")
+
 
 # Push
 source("build_scripts/push.R")
--- build_scripts/render_and_copy_quarto_delta.R
+++ build_scripts/render_and_copy_quarto_delta.R
@@ -37,6 +37,7 @@ dir.create("docs/quarto/applied_analytics")
 dir.create("docs/quarto/data_analytics")
 dir.create("docs/quarto/digitization_and_programming")
 dir.create("docs/quarto/digitization_and_programming/script")
+dir.create("docs/quarto/digitization_and_programming/exercises")
 
 # Copy PDF files
 file.copy(pdf_list, pdf_dest_list, overwrite = TRUE)
